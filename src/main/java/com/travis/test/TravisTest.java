7a0065d9bdbad6f8d63a94c1053c6c8d8290dba9
sellanma01 <sellanma01@DAYWORKW7865.enterprisenet.org>
2018-03-01 23:23:08 -0800

    Travis Test Commit

---
+++ src/main/java/com/travis/test/TravisTest.java
@@ -0,0 +1,12 @@
+package com.travis.test;
+
+public class TravisTest {
+
+	public static void main(String[] args) {
+		System.out.println("Travis Test");
+	}
+	
+	public int travisTest(int a , int b) {
+		return a + b;
+	}
+}
